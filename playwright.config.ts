1d2c9048e3624388aaf945122235747ff6c8e4e9
Keerthi Panneerselvam <keerthi.panneerselvam@ideas2it.com>
2025-04-29 12:42:37 +0530

    Initial commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,34 @@
-import { defineConfig } from '@playwright/test';
+import {defineConfig, devices} from "@playwright/test";
+
 
 export default defineConfig({
-  testDir: './tests',
-  use: {
-    baseURL: 'https://the-internet.herokuapp.com',
-    headless: true,
-    viewport: { width: 1280, height: 720 },
-    actionTimeout: 5000,
-    navigationTimeout: 10000,
-  },
-  retries: 2, 
-  reporter: [['html', { open: 'never' }]]
+    testDir: "./tests",
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 1 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: process.env.CI ? undefined : 6,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    //reporter: [["html"], ["blob"]],
+    reporter: process.env.CI ? "blob" : "html",
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: "on",
+        screenshot: "on",
+        video: "on",
+    },
+
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: "chromium",
+            use: {...devices["Desktop Chrome"]},
+        },
+
+    ],
+
 });
